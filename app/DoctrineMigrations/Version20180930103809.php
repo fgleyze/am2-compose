b26401f52260244e6b59374cc33ce196fd85c3b8
francois gleyze <francois.gleyze@lrqdo.fr>
2018-09-30 14:09:45 +0200

    Add migration

---
+++ app/DoctrineMigrations/Version20180930103809.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180930103809 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE agency ADD facebook VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE agency ADD instagram VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE agency ADD pinterest VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE agency DROP facebook');
+        $this->addSql('ALTER TABLE agency DROP instagram');
+        $this->addSql('ALTER TABLE agency DROP pinterest');
+    }
+}
